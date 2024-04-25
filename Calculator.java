853b18783cfccf0e6aeca16c4988b15b1d344f25
Vyacheslav Lukin <dwek18@mail.ru>
2024-04-25 17:44:04 +0300

    Change Main

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,7 @@
-package HomeWork.Lambda_Expressions_and_Functional_Interfaces.Calculator;
-
 import java.util.function.*;
 
 public class Calculator {
+    
     static Supplier<Calculator> instance = Calculator::new;
 
     Calculator() {
@@ -11,16 +10,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    //    BinaryOperator<Integer> devide = (x, y) -> x / y;
-    BinaryOperator<Integer> devide = (x, y) -> {
-
-        try {
-            return x / y;
-        } catch (ArithmeticException e){
-            System.out.println("На ноль делить нельзя ");
-        return 0;
-        }
-    };
+    BinaryOperator<Integer> devide = (x, y) -> x / y;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- Main.java
+++ Main.java
@@ -1,21 +1,18 @@
-package HomeWork.Lambda_Expressions_and_Functional_Interfaces.Calculator;
-
-import java.util.function.Predicate;
-
 public class Main {
     public static void main(String[] args) {
+        
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-//        int c = 0;
+        int c = 0;
 
-//        try {
-        int c = calc.devide.apply(a, b);
-//        } catch (ArithmeticException e) {
-//            System.out.println("На ноль делить нельзя");
-//        }
-
+        try {
+            c = calc.devide.apply(a, b);
+        } catch (ArithmeticException e) {
+            System.out.println("На ноль делить нельзя");
+        }
+        
         // TODO : Причина возникновения ошибки: на 0 делить нельзя,
         // TODO : так как ArithmeticException наследуется от RuntimeException, то ошибка выходит
         // TODO : во время выполнения программы. Runtime исключения бывают в коде, в котором присутствуют
